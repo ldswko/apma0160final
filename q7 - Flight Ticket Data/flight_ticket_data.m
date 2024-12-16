fce7ebc10c170b1cb0b2867fb504bd6a2a1fae9c
Lloyd Sangwoo Ko <sangwoo_ko@brown.edu>
2024-12-16 13:45:28 -0500

    Update flight_ticket_data.m

--- q7 - Flight Ticket Data/flight_ticket_data.m
+++ q7 - Flight Ticket Data/flight_ticket_data.m
@@ -1,42 +1,50 @@
 load price_data.mat
 
 t_plot = linspace(times(1), times(end), 1000);
-A1 = [times, ones(size(times))];
-A2 = [times.^2, times, ones(size(times))]; 
-A5 = [times.^5, times.^4, times.^3, times.^2, times, ones(size(times))]; 
 
-coeffs_1 = A1 \ prices; 
-coeffs_2 = A2 \ prices;
-coeffs_5 = A5 \ prices; 
+n = length(times);
+A_1 = ones(n,2);
+A_2 = ones(n,3);
+A_5 = ones(n,6);
 
-c1 = coeffs_2(1);
-c2 = coeffs_2(2);
-c3 = coeffs_2(3);
+for i = 1:5
+    A_5(:, i) = times.^(6-i);
+end
 
-best_time = -c2 / (2 * c1);
+for j = 1:2
+    A_2(:, j) = times.^(3-j);
+end
 
-best_price = c1 * best_time^2 + c2 * best_time + c3;
+for k = 1:1
+    A_1(:, k) = times.^(2-k);
+end
 
-p1 = coeffs_1(1) + coeffs_1(2) * t_plot; 
-p2 = coeffs_2(1) * t_plot.^2 + coeffs_2(2) * t_plot + coeffs_2(3); 
-p5 = coeffs_5(1) * t_plot.^5 + coeffs_5(2) * t_plot.^4 + ...
-     coeffs_5(3) * t_plot.^3 + coeffs_5(4) * t_plot.^2 + ...
-     coeffs_5(5) * t_plot + coeffs_5(6); 
+coeffs_1 = A_1\prices;
+coeffs_2 = A_2\prices;
+coeffs_5 = A_5\prices;
 
-figure;
-plot(times, prices, 'k.', 'MarkerSize', 15); 
+best_time = -0.5 * (coeffs_2(2) / coeffs_2(1));
+best_price = polyval(coeffs_2, best_time);
+
+plotA1 = polyval(coeffs_1, t_plot);
+plotA2 = polyval(coeffs_2, t_plot);
+plotA5 = polyval(coeffs_5, t_plot);
+
+plot(times, prices, 'k.', 'MarkerSize', 20) 
 hold on;
-plot(best_time, best_price, 'ro', 'MarkerSize', 10, 'LineWidth', 2); 
-plot(t_plot, p1, 'b--', 'LineWidth', 1.5); 
-plot(t_plot, p2, 'g-', 'LineWidth', 2); 
-plot(t_plot, p5, 'm-.', 'LineWidth', 1.5); 
+plot(best_time, best_price, 'm.', 'MarkerSize', 20); 
+hold on;
+plot(t_plot, plotA1, 'b', 'LineWidth', 1); 
+hold on;
+plot(t_plot, plotA2, 'r', 'LineWidth', 1); 
+hold on;
+plot(t_plot, plotA5, 'g', 'LineWidth', 1); 
 
-legend('Price Data (Observed)', 'Optimal Purchase Point', 'Linear Fit (Degree 1)', ...
-       'Quadratic Fit (Degree 2)', 'Polynomial Fit (Degree 5)', 'Location', 'best');
-xlabel('Time (days before flight)');
-ylabel('Price ($)');
-title('Ticket Prices vs. Time with Polynomial Approximations');
-grid on; 
+legend('Observed Prices', 'Optimal Time/Price', 'Linear Fit (Degree 1)', ...
+    'Quadratic Fit (Degree 2)', 'Polynomial Fit (Degree 5)', ...
+    'FontSize', 12, 'Location', 'Best');
+
+xlabel('Times (Days)', 'FontSize', 15);
+ylabel('Prices (Dollars)', 'FontSize', 15);
+
 hold off;
-
-save('flight_ticket.data.m', 'A1', 'A2', 'A5', 'coeffs_1', 'coeffs_2', 'coeffs_5', 'best_time', 'best_price');
